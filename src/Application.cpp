017f2a304c75530912a7ce183e15341cee975e8d
Сергей Макарычев <sm05112001@gmail.com>
2023-08-15 21:43:36 +0700

    bruh

--- src/Application.cpp
+++ src/Application.cpp
@@ -454,7 +454,7 @@ void Application::CreateVertexBuffer()
     m_VertexBuffer = CreateBuffer(
         vertexBufferSizeBytes,
         VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
     );
 
     CopyBuffer(stageBufferData.Buffer, m_VertexBuffer.Buffer, vertexBufferSizeBytes);
