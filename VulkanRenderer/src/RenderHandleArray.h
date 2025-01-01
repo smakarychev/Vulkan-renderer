c959ba388e1bb77a0b062e0bc52a816c8c356dbb
smakarychev <49116143+smakarychev@users.noreply.github.com>
2025-01-02 01:08:26 +0700

    Added "insert" to renderHandleArray, removed unique_ptr from it

--- VulkanRenderer/src/RenderHandleArray.h
+++ VulkanRenderer/src/RenderHandleArray.h
@@ -18,19 +18,19 @@ public:
         using pointer = value_type*;
         using reference = value_type&;
 
-        Iterator(std::vector<std::unique_ptr<T>>* elements, IdType index)
+        Iterator(std::vector<T>* elements, IdType index)
             : m_Elements(elements), m_Index(index) {}
-        Iterator(const std::vector<std::unique_ptr<T>>* elements, IdType index)
+        Iterator(const std::vector<T>* elements, IdType index)
             : m_Elements(elements), m_Index(index) {}
             
-        reference operator*() const { return *m_Elements->operator[](m_Index); }
-        pointer operator->() { return m_Elements->operator[](m_Index).get(); }
+        reference operator*() const { return m_Elements->operator[](m_Index); }
+        pointer operator->() { return std::addressof(m_Elements->operator[](m_Index)); }
         Iterator& operator++() { ++m_Index; return *this; }
         Iterator operator++(i32) { Iterator tmp = *this; ++(*this); return tmp; }
         friend bool operator==(const Iterator& a, const Iterator& b) { return a.m_Index == b.m_Index; }
         friend bool operator!=(const Iterator& a, const Iterator& b) { return !(a == b); }
     private:
-        const std::vector<std::unique_ptr<T>>* m_Elements;
+        const std::vector<T>* m_Elements;
         IdType m_Index;
     };
     using iterator = Iterator;
@@ -41,22 +41,27 @@ public:
     RenderHandle<T> push_back(T&& val);
 
     void Pop() { m_Elements.pop_back(); }
-    void pop_back() { m_Elements.pop_back(); }
+    void pop_back() { Pop(); }
+
+    RenderHandle<T> Insert(u32 position, const T& val);
+    RenderHandle<T> Insert(u32 position, T&& val);
+    RenderHandle<T> insert(u32 position, const T& val);
+    RenderHandle<T> insert(u32 position, T&& val);
 
     u32 Size() const { return (u32)m_Elements.size(); }
-    u32 size() const { return (u32)m_Elements.size(); }
+    u32 size() const { return Size(); }
 
     bool Empty() const { return size() == 0; }
-    bool empty() const { return size() == 0; }
+    bool empty() const { return Empty(); }
 
     void Reserve(u32 size) { m_Elements.reserve(size); }
-    void reserve(u32 size) { m_Elements.reserve(size); }
+    void reserve(u32 size) { Reserve(size); }
 
     void Resize(u32 size) { m_Elements.resize(size); }
-    void resize(u32 size) { m_Elements.resize(size); }
+    void resize(u32 size) { Resize(size); }
 
-    const T& operator[](RenderHandle<T> handle) const { return *m_Elements[Id(handle)]; }
-    T& operator[](RenderHandle<T> handle) { return *m_Elements[Id(handle)]; }
+    const T& operator[](RenderHandle<T> handle) const { return m_Elements[Id(handle)]; }
+    T& operator[](RenderHandle<T> handle) { return m_Elements[Id(handle)]; }
 
     Iterator begin() { return Iterator{&m_Elements, 0}; }
     Iterator end() { return Iterator{&m_Elements, (u32)m_Elements.size()}; }
@@ -65,18 +70,18 @@ public:
     Iterator end() const { return Iterator{&m_Elements, (u32)m_Elements.size()}; }
 
     IdType IndexOf(RenderHandle<T> handle) const { return Id(handle); }
-    IdType index_of(RenderHandle<T> handle) const { return Id(handle); }
+    IdType index_of(RenderHandle<T> handle) const { return IndexOf(handle); }
 private:
     static IdType Id(RenderHandle<T> handle) { return handle.m_Id; }
 private:
-    std::vector<std::unique_ptr<T>> m_Elements;
+    std::vector<T> m_Elements;
 };
 
 template <typename T>
 RenderHandle<T> RenderHandleArray<T>::Push(const T& val)
 {
     RenderHandle<T> toReturn = (u32)m_Elements.size();
-    m_Elements.push_back(std::make_unique<T>(val));
+    m_Elements.push_back(val);
 
     return toReturn;
 }
@@ -85,7 +90,7 @@ template <typename T>
 RenderHandle<T> RenderHandleArray<T>::Push(T&& val)
 {
     RenderHandle<T> toReturn = (u32)m_Elements.size();
-    m_Elements.push_back(std::make_unique<T>(std::forward<T>(val)));
+    m_Elements.push_back(std::forward<T>(val));
 
     return toReturn;
 }
@@ -93,17 +98,41 @@ RenderHandle<T> RenderHandleArray<T>::Push(T&& val)
 template <typename T>
 RenderHandle<T> RenderHandleArray<T>::push_back(const T& val)
 {
-    RenderHandle<T> toReturn = (u32)m_Elements.size();
-    m_Elements.push_back(std::make_unique<T>(val));
-
-    return toReturn;
+    return Push(val);
 }
 
 template <typename T>
 RenderHandle<T> RenderHandleArray<T>::push_back(T&& val)
 {
-    RenderHandle<T> toReturn = (u32)m_Elements.size();
-    m_Elements.push_back(std::make_unique<T>(std::forward<T>(val)));
+    return Push(std::forward<T>(val));
+}
+
+template <typename T>
+RenderHandle<T> RenderHandleArray<T>::Insert(u32 position, const T& val)
+{
+    RenderHandle<T> toReturn = position;
+    m_Elements.insert(m_Elements.begin() + position, val);
 
     return toReturn;
+}
+
+template <typename T>
+RenderHandle<T> RenderHandleArray<T>::Insert(u32 position, T&& val)
+{
+    RenderHandle<T> toReturn = position;
+    m_Elements.insert(m_Elements.begin() + position, std::forward<T>(val));
+
+    return toReturn;
+}
+
+template <typename T>
+RenderHandle<T> RenderHandleArray<T>::insert(u32 position, const T& val)
+{
+    return Insert(position, val);
+}
+
+template <typename T>
+RenderHandle<T> RenderHandleArray<T>::insert(u32 position, T&& val)
+{
+    return Insert(position, std::forward<T>(val));
 }
