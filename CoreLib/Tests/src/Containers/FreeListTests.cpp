319e48a52ceb0bd2a8cccc0b5d7f3ce4e383b045
smakarychev <49116143+smakarychev@users.noreply.github.com>
2026-02-23 23:40:01 +0700

    freelist container

---
+++ CoreLib/Tests/src/Containers/FreeListTests.cpp
@@ -0,0 +1,65 @@
+﻿#include "catch2/catch_test_macros.hpp"
+
+#include "Containers/FreeList.h"
+
+// NOLINTBEGIN
+
+TEST_CASE("FreeList", "[Containers][FreeList]")
+{
+    SECTION("Can create for trivially destructable types")
+    {
+        lux::FreeList<u32> listA;
+
+        struct Test
+        {
+            u32 Element;
+        };
+        lux::FreeList<Test> listB;
+    }
+    SECTION("Can add elements")
+    {
+        lux::FreeList<u32> list;
+        REQUIRE(list.Size() == 0);
+        list.Add(1);
+        REQUIRE(list.Size() == 1);
+        list.Add(2);
+        REQUIRE(list.Size() == 2);
+    }
+    SECTION("Can remove elements")
+    {
+        lux::FreeList<u32> list;
+        REQUIRE(list.Size() == 0);
+        const u32 index = list.Add(1);
+        REQUIRE(list.Size() == 1);
+        list.Remove(index);
+        REQUIRE(list.Size() == 0);
+    }
+    SECTION("Remove does not change capacity")
+    {
+        lux::FreeList<u32> list;
+        REQUIRE(list.Size() == 0);
+        const u32 index = list.Add(1);
+        REQUIRE(list.Capacity() == 1);
+        list.Remove(index);
+        REQUIRE(list.Capacity() == 1);
+    }
+    SECTION("Can reuse index after remove")
+    {
+        lux::FreeList<u32> list;
+        REQUIRE(list.Size() == 0);
+        u32 index = list.Add(1);
+        u32 index2 = list.Add(1);
+        REQUIRE(index != index2);
+        list.Remove(index);
+        REQUIRE(list.Add(1) == index);
+    }
+    SECTION("Can get by index")
+    {
+        lux::FreeList<u32> list;
+        REQUIRE(list.Size() == 0);
+        u32 index = list.Add(13);
+        REQUIRE(list[index] == 13);
+    }
+}
+
+// NOLINTEND
---
+++ CoreLib/src/Containers/FreeList.h
@@ -0,0 +1,124 @@
+﻿#pragma once
+#include "core.h"
+#include "types.h"
+
+namespace lux
+{
+template <typename T>
+class FreeList
+{
+    static constexpr u32 NO_FREE = ~0u;
+    using FreeIndexType = u32;
+    static_assert(sizeof(T) >= sizeof(FreeIndexType), "Cannot use this type in the freelist");
+    static_assert(std::is_trivially_destructible_v<T>, "Type must be trivially destructible");
+public:
+    ~FreeList();
+    
+    template <typename ... Args>
+    constexpr u32 Add(Args&&... args);
+    template <typename ... Args>
+    constexpr u32 add(Args&&... args) { return Add(std::forward<Args>(args)...); }
+    constexpr void Remove(u32 index);
+    constexpr void remove(u32 index) { Remove(index); }
+
+    // size of elements array, including the deleted ones
+    constexpr u32 Capacity() const { return u32(m_DataCurrent - m_DataStart); }
+    constexpr u32 capacity() const { return Capacity(); }
+    constexpr u32 Size() const { return m_Size; }
+    constexpr u32 size() const { return Size(); }
+    
+    constexpr const T& operator[](u32 index) const;
+    constexpr T& operator[](u32 index);
+private:
+    template <typename ... Args>
+    constexpr void EmplaceBack(Args&&... args);
+
+    constexpr void Resize(u32 oldSize, u32 newSize);
+private:
+    T* m_DataStart{nullptr};    
+    T* m_DataEnd{nullptr};    
+    T* m_DataCurrent{nullptr};    
+    
+    u32 m_FirstFree{NO_FREE};
+    u32 m_Size{0};
+};
+
+template <typename T>
+FreeList<T>::~FreeList()
+{
+    // type is is_trivially_destructible_v, so this should not leak
+    delete[] (u8*)m_DataStart;
+}
+
+template <typename T>
+template <typename ... Args>
+constexpr u32 FreeList<T>::Add(Args&&... args)
+{
+    u32 index;  
+    if (m_FirstFree != NO_FREE)
+    {
+        index = m_FirstFree;
+        m_FirstFree = *(const u32*)std::addressof(m_DataStart[index]);
+        std::construct_at(std::addressof(m_DataStart[index]), std::forward<Args>(args)...);
+    }
+    else
+    {
+        index = Capacity();
+        EmplaceBack(std::forward<Args>(args)...);
+    }
+
+    m_Size++;
+    
+    return index;
+}
+
+template <typename T>
+constexpr void FreeList<T>::Remove(u32 index)
+{
+    ASSERT(index < Capacity(), "Element handle out of bounds")
+
+    *(u32*)std::addressof(m_DataStart[index]) = m_FirstFree;
+    m_FirstFree = index;
+    
+    m_Size--;
+}
+
+template <typename T>
+constexpr const T& FreeList<T>::operator[](u32 index) const
+{
+    ASSERT(index < Capacity(), "Element handle out of bounds")
+
+    return m_DataStart[index];
+}
+
+template <typename T>
+constexpr T& FreeList<T>::operator[](u32 index)
+{
+    return const_cast<T&>(const_cast<const FreeList&>(*this)[index]);
+}
+
+template <typename T>
+template <typename ... Args>
+constexpr void FreeList<T>::EmplaceBack(Args&&... args)
+{
+    if (m_DataCurrent == m_DataEnd)
+    {
+        const u32 size = (u32)(m_DataEnd - m_DataStart);
+        Resize(size, (u32)((f32)(size + 1) * 1.5f));
+    }
+    std::construct_at(m_DataCurrent, std::forward<Args>(args)...);
+    m_DataCurrent++;
+}
+
+template <typename T>
+constexpr void FreeList<T>::Resize(u32 oldSize, u32 newSize)
+{
+    T* newData = (T*)new u8[newSize * sizeof(T)];
+    std::memcpy((void*)newData, (void*)m_DataStart, sizeof(T) * oldSize);
+    
+    delete[] (u8*)m_DataStart;
+    m_DataStart = newData;
+    m_DataCurrent = m_DataStart + oldSize;
+    m_DataEnd = m_DataStart + newSize;
+}
+}
