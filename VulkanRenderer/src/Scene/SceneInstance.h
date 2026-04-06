c2b9fcbf5de4ccdc2df33d3f4cdf5889fcf6c580
smakarychev <49116143+smakarychev@users.noreply.github.com>
2026-04-06 22:49:06 +0700

    hash and comparison for SceneInstance

--- VulkanRenderer/src/Scene/SceneInstance.h
+++ VulkanRenderer/src/Scene/SceneInstance.h
@@ -10,7 +10,22 @@ class SceneInstance
     friend class SceneLight;
     friend class SceneGeometry;
     friend class SceneRenderObjectSet;
-
+    friend struct std::hash<SceneInstance>;
+public:
+    auto operator<=>(const SceneInstance&) const = default;
+private:
     u32 m_InstanceId{0};
     const SceneInfo* m_SceneInfo{};
 };
+
+namespace std
+{
+template <>
+struct hash<SceneInstance>
+{
+    usize operator()(const SceneInstance instance) const noexcept
+    {
+        return std::hash<u32>{}(instance.m_InstanceId);
+    }
+};
+}
