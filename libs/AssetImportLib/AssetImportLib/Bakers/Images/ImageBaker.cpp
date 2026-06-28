23217a9c323078fcca493af5e1a912613b695250
smakarychev <49116143+smakarychev@users.noreply.github.com>
2026-06-28 12:40:38 +0700

    removed stbi_set_flip_vertically_on_load
    seems like I don't need to flip jpeg images

--- libs/AssetImportLib/AssetImportLib/Bakers/Images/ImageBaker.cpp
+++ libs/AssetImportLib/AssetImportLib/Bakers/Images/ImageBaker.cpp
@@ -418,7 +418,6 @@ IoResult<assetlib::ImageAsset> readUncompressedLdr(const assetlib::ImageMeta& me
 {
     i32 width, height, channels;
     u8* pixels{nullptr};
-    stbi_set_flip_vertically_on_load(true);
     pixels = (u8*)stbi_load(meta.Metadata.Io.OriginalFile.c_str(), &width, &height, &channels, STBI_rgb_alpha);
     const u64 sizeBytes = (u64)width * (u64)height * 4llu;
     std::vector imageData((std::byte*)pixels, (std::byte*)pixels + sizeBytes);
